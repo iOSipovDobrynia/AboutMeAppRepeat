c893decb35d2aec1260d47b457273f630c9d90bd
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-08-01 22:48:07 +0300

    Initial Commit

---
+++ AboutMeAppRepeat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeAppRepeat
+//
+//  Created by Goodwasp on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
